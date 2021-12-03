a6b8b67e98ba79b324d6e77a1d3d63fd4e792b6b
lbm364dl <geometrydashm364@gmail.com>
2021-12-03 12:07:36 +0100

    clean

--- d03/d03.hs
+++ d03/d03.hs
@@ -5,10 +5,10 @@ mostCommon :: [S.Seq Int] -> Int -> Int
 mostCommon xs j = fromEnum (2 * s >= n)
   where
       n = length xs
-      s = sum $ map (flip S.index j) xs
+      s = sum [x `S.index` j | x <- xs]
 
 iter :: Int -> [S.Seq Int] -> Int -> [S.Seq Int]
-iter least xs j = filter ((== which) . (flip S.index j)) xs
+iter least xs j = filter (\x -> x `S.index` j == which) xs
   where
       which = mostCommon xs j `xor` least
 
@@ -19,7 +19,7 @@ star1 :: [S.Seq Int] -> Int
 star1 xs = x * negX
   where
       m = S.length $ head xs
-      x = toInt . S.fromList $ foldr (\x acc -> (mostCommon xs x):acc) [] [0..m-1]
+      x = toInt . S.fromList . foldr (\x acc -> (mostCommon xs x):acc) [] $ [0..m-1]
       negX = (shift 1 m) - 1 - x
 
 star2 :: [S.Seq Int] -> Int
@@ -28,8 +28,8 @@ star2 xs = (res 0) * (res 1)
       m = S.length $ head xs
       f least acc x
           | length acc > 1 = iter least acc x
-          | otherwise = acc
-      res least = toInt . head $ foldl (f least) xs [0..m-1]
+          | otherwise      = acc
+      res least = toInt . head . foldl (f least) xs $ [0..m-1]
 
 main = do
     inp <- readFile "input.txt"
