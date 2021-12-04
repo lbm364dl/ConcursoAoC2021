b24ca11d7f9ebfab67862c1955196b59912d2d4f
lbm364dl <geometrydashm364@gmail.com>
2021-12-04 15:00:06 +0100

    clean

--- d04/d04.hs
+++ d04/d04.hs
@@ -15,10 +15,10 @@ tables xs = tables' xs 5
       tables' (_:xs) 0 = [] : tables' xs 5
       tables' (x:xs) i = let (h:t) = tables' xs (i-1) in ((map read $ words x):h) : t
 
-win :: Nums -> Board -> (Time, Score)
-win xs b = win' b xs S.empty 0
+win :: Nums -> Int -> Board -> (Time, Score)
+win xs def b = win' b xs S.empty 0
   where
-      win' _ [] _ _ = (10000, 0)
+      win' _ [] _ _ = (def, 0)
       win' b (x:xs) s t
         | check b x newS = (t, x * (score b newS))
         | otherwise = win' b xs newS (t+1)
@@ -26,13 +26,7 @@ win xs b = win' b xs S.empty 0
               newS = S.insert (pos b x) s
 
 pos :: Board -> Int -> (Int, Int)
-pos b x = pos' b x 0 0
-  where
-      pos' [] _ _ _ = (-1, -1)
-      pos' ([]:xs) x i j = pos' xs x (i+1) 0
-      pos' ((y:ys):xs) x i j 
-        | x == y = (i, j) 
-        | otherwise = pos' (ys:xs) x i (j+1)
+pos b x = head $ [(i, j) | (xs, i) <- zip b [0..4], (el, j) <- zip xs [0..4], el == x] ++ [(-1, 1)]
 
 check :: Board -> Int -> S.Set (Int, Int) -> Bool
 check b x s = checkRow || checkCol
@@ -41,19 +35,15 @@ check b x s = checkRow || checkCol
       checkCol = any (\j -> all (\i -> (i,j) `S.member` s) [0..4]) [0..4]
 
 score :: Board -> S.Set (Int, Int) -> Int
-score b s = score' b s 0 0
-  where
-      score' [] _ _ _ = 0
-      score' ([]:xs) s i j = score' xs s (i+1) 0
-      score' ((y:ys):xs) s i j = (if (i,j) `S.notMember` s then y else 0) + (score' (ys:xs) s i (j+1))
+score b s = sum [el | (xs, i) <- zip b [0..4], (el, j) <- zip xs [0..4], (i, j) `S.notMember` s]
 
-ans :: [Board] -> Nums -> ([(Int, Int)] -> (Int, Int))-> Int
-ans b xs cmp = snd . cmp . map (win xs) $ b
+ans :: [Board] -> Nums -> ([(Int, Int)] -> (Int, Int)) -> Int -> Int
+ans b xs cmp def = snd . cmp . map (win xs def) $ b
 
 main = do
     inp <- readFile "input.txt"
     let ls = lines inp
     let nums = map read (split $ head ls) :: [Int]
     let tabs = tables (drop 2 ls)
-    putStrLn $ "Star 1: " ++ (show $ ans tabs nums minimum)
-    putStrLn $ "Star 2: " ++ (show $ ans tabs nums maximum)
+    putStrLn $ "Star 1: " ++ (show $ ans tabs nums minimum 10000)
+    putStrLn $ "Star 2: " ++ (show $ ans tabs nums maximum (-1))
