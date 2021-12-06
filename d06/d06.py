263c54caac3fa4a70ad16c98b7e8e2beb958cea9
lbm364dl <geometrydashm364@gmail.com>
2021-12-06 10:19:38 +0100

    clean

--- d06/d06.py
+++ d06/d06.py
@@ -1,10 +1,10 @@
 def iter(its, inp):
-    d = {i : inp.count(i) for i in range(9)}
+    l = [inp.count(i) for i in range(9)]
     for _ in range(its):
-        d = {i : d[(i+1)%9] for i in range(9)}
-        d[6] += d[8]
+        l = [l[(i+1)%9] for i in range(9)]
+        l[6] += l[8]
 
-    return sum(d.values())
+    return sum(l)
 
 *inp, = map(int, open('input.txt').read().split(','))
 print('Star 1:', iter(80, inp))
