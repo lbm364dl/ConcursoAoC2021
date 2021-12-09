701c59693e27bb39302d5aa6d0f7bf6a2b2266cb
lbm364dl <geometrydashm364@gmail.com>
2021-12-09 12:39:50 +0100

    clean

--- d09/d09.py
+++ d09/d09.py
@@ -9,16 +9,15 @@ def is_low_point(y, x):
     return all(is_outside(y+dy, x+dx) or grid[y+dy][x+dx] > grid[y][x] for dy, dx in delta)
 
 def dfs(y, x):
-    if is_outside(y, x) or grid[y][x] == 9 or (y,x) in visited:
+    if is_outside(y, x) or grid[y][x] == 9:
         return 0
 
-    visited.add((y, x))
+    grid[y][x] = 9
     return 1 + sum(dfs(y+dy, x+dx) for dy, dx in delta)
 
 
-lows = [(y, x) for y in range(n) for x in range(m) if is_low_point(y, x)]
-visited = set()
-basins = sorted([dfs(y, x) for y, x in lows])
+lows = [(y, x, grid[y][x]) for y in range(n) for x in range(m) if is_low_point(y, x)]
+b1, b2, b3 = sorted([dfs(y, x) for y, x, _ in lows])[-3:]
 
-print('Star 1:', sum(grid[y][x] + 1 for y, x in lows))
-print('Star 2:', basins[-1] * basins[-2] * basins[-3])
+print('Star 1:', sum(val + 1 for _, _, val in lows))
+print('Star 2:', b1 * b2 * b3)
