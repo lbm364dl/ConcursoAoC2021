2b81db96be127ca00c7bbb0497976235900c8d68
lbm364dl <geometrydashm364@gmail.com>
2021-12-19 17:11:23 +0100

    still ugly but better

--- d19/d19.py
+++ d19/d19.py
@@ -1,23 +1,21 @@
-import re
-from copy import deepcopy
 import numpy as np
 from numpy.linalg import matrix_power
 from collections import defaultdict
 
-inp = [np.array([[*map(int, l.split(','))] for l in scanner.strip().splitlines()], dtype = object) for scanner in re.split(r'--- scanner \d+ ---', open('input.txt').read())[1:]]
-n = len(inp)
+# very slow, takes 2 mins
 
-done = {0}
-absolute = {i : [] for i in range(n)}
-absolute[0] = inp[0]
-scanners = {0 : np.array([0,0,0])}
+inp = [np.array([[*map(int, p.split(','))] for p in sc.splitlines()[1:]]) for sc in open('input.txt').read().split('\n\n')]
+n = len(inp)
+left = set(range(1, n))
+absolute = [inp[0], *[[] for i in range(1, n)]]
+scan = [np.array([0, 0, 0]), *[[] for i in range(1, n)]]
 
 ops = []
 # clockwise 90deg rotation on yz plane (x fixed)
-rot1 = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]], dtype = object)
+rot1 = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
 # clockwise 90deg rotation on xz plane (y fixed)
-rot2 = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]], dtype = object)
-
+rot2 = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])
+# all orientations can be obtained as compositions of those two rotations
 for i in range(4):
     for j in range(4):
         for k in range(4):
@@ -26,47 +24,28 @@ for i in range(4):
                 if not any((op == m).all() for op in ops):
                     ops.append(m)
 
-def transform(i, ps):
-    return np.array([ops[i].dot(p) for p in ps])
 
-q = {0}
-while len(done) < n:
-    i = q.pop()
-    print(done, i, q)
-    for j in range(n):
-        if j in done:
-            continue
+def compare(i, j, q, left):
+   cnt = defaultdict(int)
+   for p1 in absolute[i]:
+       for k in range(24):
+           transformed = ops[k].dot(inp[j].transpose()).transpose()
+           for p2 in transformed:
+               cnt[tuple(p1-p2)] += 1
 
-        found = False
-        cnt = defaultdict(int)
-        for p1 in absolute[i]:
-            for k in range(24):
-                transformed = transform(k, inp[j])
-#                print(transformed)
-                for p2 in transformed:
-                    cnt[tuple(p1-p2)] += 1
-#                if j == 1:
-#                    print([cnt[k] for k in cnt if cnt[k] > 1])
-                for d, size in cnt.items():
-                    if size >= 12:
-                        scanners[j] = d
-                        absolute[j] = d + transformed
-                        q.add(j)
-                        done.add(j)
-                        found = True
-                        break
-                if found: break
-            if found: break
+           for d, size in cnt.items():
+               if size >= 12:
+                   scan[j] = np.array(d, dtype = object)
+                   absolute[j] = d + transformed
+                   q.append(j)
+                   left.remove(j)
+                   return
 
-s = set()
-for k in done:
-    for p in absolute[k]:
-        s.add(tuple(p))
+# bfs on scanners with absolute position found
+q = [0]
+for i in q:
+    for j in list(left):
+        compare(i, j, q, left)
 
-def manhattan(p1, p2):
-    return sum(abs(p1[i]-p2[i]) for i in range(3))
-
-print(max(manhattan(scanners[i], scanners[j]) for i in range(n) for j in range(i+1, n)))
-print(len(s))
-#print({tuple(p) for ps in absolute for p in ps})
-
+print('Star 1:', len({tuple(p) for ps in absolute for p in ps}))
+print('Star 2:', max(sum(map(abs, scan[i]-scan[j])) for i in range(n) for j in range(i+1, n)))
